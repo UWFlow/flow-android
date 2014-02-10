750ab3fc8cce7046a32c8e627b3f6fdb5f5cf741
ChinmayR <chinmayraval@hotmail.com>
2014-02-10 18:02:09 -0500

    Created AlarmClockService basic functionality

--- src/com/uwflow/flow_android/MyActivity.java
+++ src/com/uwflow/flow_android/MyActivity.java
@@ -1,7 +1,12 @@
 package com.uwflow.flow_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import com.uwflow.flow_android.services.AlarmClockService;
+import android.util.Log;
+
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +16,17 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        AlarmClockService alarmService = new AlarmClockService();
+        startActivity(alarmService.setAlarmForTimeWithMessage("test",5,55,2));
+
+        /*
+        Intent alarmClockSetIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
+        alarmClockSetIntent.putExtra(AlarmClock.EXTRA_MESSAGE, "test");
+        alarmClockSetIntent.putExtra(AlarmClock.EXTRA_HOUR, 15);
+        alarmClockSetIntent.putExtra(AlarmClock.EXTRA_MINUTES, 37);
+        startActivity(alarmClockSetIntent);
+        */
+
     }
 }
