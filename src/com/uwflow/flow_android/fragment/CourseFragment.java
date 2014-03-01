7a9e147ea4169a751b32172a1a6d2af1885b6acf
Jasper Fung <email@jasperfung.com>
2014-03-01 18:02:29 -0500

    [course] Add share functionality to Course pages (todo: create valid links to share)

--- src/com/uwflow/flow_android/fragment/CourseFragment.java
+++ src/com/uwflow/flow_android/fragment/CourseFragment.java
@@ -1,5 +1,6 @@
 package com.uwflow.flow_android.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,6 +9,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.astuetz.PagerSlidingTabStrip;
 import com.uwflow.flow_android.R;
 import com.uwflow.flow_android.constant.Constants;
@@ -37,6 +39,28 @@ public class CourseFragment extends Fragment {
 
         // Set default tab to About
         mViewPager.setCurrentItem(Constants.COURSE_ABOUT_PAGE_INDEX);
+
+        Button shortlistButton = (Button)rootView.findViewById(R.id.shortlist_btn);
+        shortlistButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Add code to shortlist a course
+            }
+        });
+
+
+        Button shareButton = (Button)rootView.findViewById(R.id.share_btn);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                // TODO: we need to construct a valid share-link
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "http://www.uwflow.com");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Check out this course!");
+                startActivity(Intent.createChooser(shareIntent, "Share"));
+            }
+        });
     }
 
     @Override
