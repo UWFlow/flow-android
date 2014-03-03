2d5f68cdcb0f447f07ff50001061ba12c265c18e
Jasper Fung <email@jasperfung.com>
2014-03-02 19:37:57 -0500

    Now passing data into CourseAboutFragment and reloading the rating overview panel and description textview

--- src/com/uwflow/flow_android/fragment/CourseAboutFragment.java
+++ src/com/uwflow/flow_android/fragment/CourseAboutFragment.java
@@ -7,12 +7,11 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.*;
 import com.uwflow.flow_android.R;
 import com.uwflow.flow_android.adapters.FriendListAdapter;
 import com.uwflow.flow_android.constant.Constants;
+import com.uwflow.flow_android.entities.CourseInfo;
 import com.uwflow.flow_android.entities.Friend;
 
 import java.util.ArrayList;
@@ -25,6 +24,11 @@ public class CourseAboutFragment extends Fragment {
     private LinearLayout mFriendListContainer;
     private ViewPager mParentViewPager;
 
+    private TextView mDescTextView;
+    private LinearLayout mRatingOverviewLayout;
+
+    private CourseInfo mCourseInfo;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,6 +37,8 @@ public class CourseAboutFragment extends Fragment {
 
         mFriendListContainer = (LinearLayout)rootView.findViewById(R.id.friend_list);
         mParentViewPager = (ViewPager)getActivity().findViewById(R.id.pager);
+        mDescTextView = (TextView)rootView.findViewById(R.id.course_desc);
+        mRatingOverviewLayout = (LinearLayout) rootView.findViewById(R.id.rating_overview);
 
         // Configure "See individual reviews" button to point to the course reviews tab
         Button individualReviewsButton = (Button)rootView.findViewById(R.id.see_reviews_btn);
@@ -66,5 +72,30 @@ public class CourseAboutFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
+    }
+
+    public void loadCourseInfo(CourseInfo courseInfo) {
+        // TODO: null checking?
+        mCourseInfo = courseInfo;
+
+        mDescTextView.setText(mCourseInfo.getDescription());
+
+        reloadRatingOverview();
+    }
+
+    private void reloadRatingOverview() {
+        TextView overallRating = (TextView)mRatingOverviewLayout.findViewById(R.id.overall_rating);
+        TextView overallCount = (TextView)mRatingOverviewLayout.findViewById(R.id.overall_count);
+        ProgressBar usefulRating = (ProgressBar)mRatingOverviewLayout.findViewById(R.id.useful_bar);
+        TextView usefulCount = (TextView)mRatingOverviewLayout.findViewById(R.id.useful_count);
+        ProgressBar easyRating = (ProgressBar)mRatingOverviewLayout.findViewById(R.id.easy_bar);
+        TextView easyCount = (TextView)mRatingOverviewLayout.findViewById(R.id.easy_count);
+
+        overallRating.setText(String.format("%d%%", (int)(mCourseInfo.getOverallRating().getOverallRating() * 100)));
+        overallCount.setText(String.format("%d ratings", (int)mCourseInfo.getOverallRating().getOverallCount()));
+        usefulRating.setProgress((int)(mCourseInfo.getOverallRating().getUsefulRating() * 100));
+        usefulCount.setText(String.format("%d ratings", mCourseInfo.getOverallRating().getUsefulCount()));
+        easyRating.setProgress((int)(mCourseInfo.getOverallRating().getEasyRating() * 100));
+        easyCount.setText(String.format("%d ratings", mCourseInfo.getOverallRating().getEasyCount()));
     }
 }
--- src/com/uwflow/flow_android/fragment/CourseFragment.java
+++ src/com/uwflow/flow_android/fragment/CourseFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,13 +27,23 @@ public class CourseFragment extends Fragment implements android.support.v4.app.L
     private View rootView;
     protected ViewPager mViewPager;
     protected PagerSlidingTabStrip mTabs;
+
     private TextView mCourseCodeTextView;
     private TextView mCourseNameTextView;
+    private CourseInfo mCourseInfo;
+
+    private CourseScheduleFragment mCourseScheduleFragment;
+    private CourseAboutFragment mCourseAboutFragment;
+    private CourseReviewsFragment mCourseReviewsFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.course_layout, container, false);
+
+        mCourseScheduleFragment = new CourseScheduleFragment();
+        mCourseAboutFragment = new CourseAboutFragment();
+        mCourseReviewsFragment = new CourseReviewsFragment();
         return rootView;
     }
 
@@ -40,7 +51,7 @@ public class CourseFragment extends Fragment implements android.support.v4.app.L
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
-        mViewPager.setAdapter(new CoursePagerAdapter(getActivity().getSupportFragmentManager()));
+        mViewPager.setAdapter(new CoursePagerAdapter(getActivity().getSupportFragmentManager(), mCourseScheduleFragment, mCourseAboutFragment, mCourseReviewsFragment));
         mTabs = (PagerSlidingTabStrip) rootView.findViewById(R.id.pager_tabs);
         mTabs.setViewPager(mViewPager);
         mCourseCodeTextView = (TextView) rootView.findViewById(R.id.course_code);
@@ -88,7 +99,14 @@ public class CourseFragment extends Fragment implements android.support.v4.app.L
     @Override
     public void onLoadFinished(android.support.v4.content.Loader<CourseInfo> courseInfoLoader, CourseInfo courseInfo) {
         mCourseCodeTextView.setText(courseInfo.getCode());
+        mCourseCodeTextView.setGravity(Gravity.LEFT);
+
         mCourseNameTextView.setText(courseInfo.getName());
+        mCourseNameTextView.setGravity(Gravity.LEFT);
+
+        mCourseInfo = courseInfo;
+
+        mCourseAboutFragment.loadCourseInfo(mCourseInfo);
     }
 
     @Override
@@ -99,23 +117,31 @@ public class CourseFragment extends Fragment implements android.support.v4.app.L
 
 
     private static class CoursePagerAdapter extends FragmentStatePagerAdapter {
+        Fragment fragment1, fragment2, fragment3;
         private static final String[] TITLES = new String[] {
                 "Schedule",
                 "About",
                 "Reviews"
         };
 
-        public CoursePagerAdapter(FragmentManager fm) {
+        public CoursePagerAdapter(FragmentManager fm, Fragment fragment1, Fragment fragment2, Fragment fragment3) {
             super(fm);
+            this.fragment1 = fragment1;
+            this.fragment2 = fragment2;
+            this.fragment3 = fragment3;
         }
 
         @Override
         public Fragment getItem(int position) {
             switch (position){
-                case Constants.COURSE_SCHEDULE_PAGE_INDEX : return new CourseScheduleFragment();
-                case Constants.COURSE_ABOUT_PAGE_INDEX: return new CourseAboutFragment();
-                case Constants.COURSE_REVIEWS_PAGE_INDEX: return new CourseReviewsFragment();
-                default: return new AboutFragment();
+                case Constants.COURSE_SCHEDULE_PAGE_INDEX : return fragment1;
+//                    if (mCourseScheduleFragment == null) {
+//                        courseScheduleFragment = new CourseScheduleFragment();
+//                    };
+//                    return courseScheduleFragment;
+                case Constants.COURSE_ABOUT_PAGE_INDEX: return fragment2;
+                case Constants.COURSE_REVIEWS_PAGE_INDEX: return fragment3;
+                default: return fragment2;
             }
         }
 
