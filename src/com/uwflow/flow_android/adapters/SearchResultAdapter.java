b7ecc55ece8b84ad12929580a2f65b8559b8da2f
Jasper Fung <email@jasperfung.com>
2014-03-04 17:51:55 -0500

    Basic Explore page layout with list adapter

---
+++ src/com/uwflow/flow_android/adapters/SearchResultAdapter.java
@@ -0,0 +1,69 @@
+package com.uwflow.flow_android.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.uwflow.flow_android.R;
+import com.uwflow.flow_android.db_object.Course;
+
+import java.util.List;
+
+/**
+ * Created by jasperfung on 3/4/14.
+ */
+public class SearchResultAdapter extends BaseAdapter {
+    private List<Course> mResults;
+    private Context mContext;
+
+    public SearchResultAdapter(List<Course> results, Context context) {
+	mResults = results;
+	mContext = context;
+    }
+
+    public int getCount() {
+	return mResults.size();
+    }
+
+    public Object getItem(int arg0) {
+	return mResults.get(arg0);
+    }
+
+    public long getItemId(int position) {
+	// TODO Auto-generated method stub
+	return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+	// verify that convertView is not null
+	if (convertView == null) {
+	    // inflate a new view
+	    LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    convertView = inflater.inflate(R.layout.search_row_item, parent, false);
+	}
+
+	// Fill view with appropriate data
+	TextView first, second, third, fourth;
+
+	first = (TextView) convertView.findViewById(R.id.course_code);
+	second = (TextView) convertView.findViewById(R.id.course_name);
+	third = (TextView) convertView.findViewById(R.id.rating);
+	fourth = (TextView) convertView.findViewById(R.id.rating_count);
+
+	final Course course = mResults.get(position);
+
+	first.setText(course.getCode());
+	second.setText(course.getName());
+	Double rating = course.getOverall().getRating();
+	if (rating == null) {
+	    third.setText("---");
+	} else {
+	    third.setText(String.format("%d%%", (int)(rating * 100)));
+	}
+	fourth.setText(String.format("%d ratings", (int)(course.getOverall().getCount())));
+
+	return convertView;
+    }
+}
--- src/com/uwflow/flow_android/fragment/ExploreFragment.java
+++ src/com/uwflow/flow_android/fragment/ExploreFragment.java
@@ -5,20 +5,92 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.*;
 import com.uwflow.flow_android.R;
+import com.uwflow.flow_android.adapters.ProfileFriendAdapter;
+import com.uwflow.flow_android.adapters.SearchResultAdapter;
+import com.uwflow.flow_android.db_object.Course;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wentaoji on 2014-02-18.
  */
-public class ExploreFragment extends Fragment {
+public class ExploreFragment extends Fragment implements AdapterView.OnItemClickListener {
+    private static final int SORT_POPULAR = 0;
+    private static final int SORT_FRIENDS_TAKEN = 1;
+    private static final int SORT_EASY = 2;
+    private static final int SORT_HARD = 3;
+    private static final int SORT_COURSE_CODE = 4;
+
+    private EditText mSearchBox;
+    private Spinner mSortSpinner;
+    private CheckBox mIncludeTakenCheckBox;
+    private ListView mResultsListView;
+
+    private View rootView;
+
+    private List<Course> mSearchResultList;
+
+    private SearchResultAdapter mSearchResultAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+	rootView = inflater.inflate(R.layout.explore_layout, container, false);
+	mSearchBox = (EditText)rootView.findViewById(R.id.search_box);
+	mSortSpinner = (Spinner)rootView.findViewById(R.id.sort_spinner);
+	mIncludeTakenCheckBox = (CheckBox)rootView.findViewById(R.id.checkbox_include_taken);
+	mResultsListView = (ListView)rootView.findViewById(R.id.results_list);
 
-        return inflater.inflate(R.layout.explore_layout, container, false);
+	mSearchResultList = new ArrayList<Course>();
+	mSearchResultAdapter = new SearchResultAdapter(mSearchResultList, getActivity());
+	mResultsListView.setAdapter(mSearchResultAdapter);
+
+	mSortSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+	    @Override
+	    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		// TODO: implement result ordering / re-searching here, based on new sort preference
+		switch (position) {
+		    case SORT_POPULAR:
+			break;
+		    case SORT_FRIENDS_TAKEN:
+			break;
+		    case SORT_EASY:
+			break;
+		    case SORT_HARD:
+			break;
+		    case SORT_COURSE_CODE:
+			break;
+		}
+	    }
+
+	    @Override
+	    public void onNothingSelected(AdapterView<?> parent) {
+
+	    }
+	});
+
+	mIncludeTakenCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+	    @Override
+	    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		// TODO: implement result hiding / re-searching here, based on isChecked state
+	    }
+	});
+
+	Button searchButton = (Button)rootView.findViewById(R.id.temp_search_button);
+	searchButton.setOnClickListener(new View.OnClickListener() {
+	    @Override
+	    public void onClick(View v) {
+		String searchString = mSearchBox.getText().toString();
+
+		// TODO: fetch results for searchString, then save List<Course> result into mSearchResultList and call mSearchResultAdapter.notifyDataSetChanged()
+	    }
+	});
+
+	return rootView;
     }
 
     @Override
@@ -37,5 +109,10 @@ public class ExploreFragment extends Fragment {
                         .commit();
             }
         });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	// TODO: open course page by passing CourseFragment a Bundle with the course_id specified
     }
 }
